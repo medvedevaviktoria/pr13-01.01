d59941eb50d7273388f7d928e6b4fff2391aeea0
medvedevaviktoria <161297736+medvedevaviktoria@users.noreply.github.com>
2024-12-03 14:34:21 +0300

    Add files via upload

---
+++ pr13_1/AutoShop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr13_1
+{
+    internal class AutoShop
+    {
+        public string CarName { set; get; }
+        public int MaxSpeed { get; set; }
+        public double Cost { get; set; }
+        public byte Discount { get; set; }
+        public int ID { get; set; }
+
+        public AutoShop() { }
+
+        public AutoShop(string carName, int maxSpeed, double cost, byte discount, int iD)
+        {
+            this.CarName = carName;
+            this.MaxSpeed = maxSpeed;
+            this.Cost = cost;
+            this.Discount = discount;
+            this.ID = iD;
+        }
+
+        public override string ToString()
+        {
+            Console.OutputEncoding=System.Text.Encoding.UTF8;//для корректного отображения обозначения символа рубля
+            return String.Format("{4}\tМарка: {0}\tМакс. скорость: {1}\tЦена: {2:C}\tСкидка: {3}%",
+                this.CarName, this.MaxSpeed, this.Cost, this.Discount, this.ID);
+        
+        }
+    }
+
+    class CompInv<T>:IComparer<T>where T : AutoShop
+    {
+        public int Compare(T x, T y)
+        {  
+          if (x.Cost < y.Cost)
+             return 1;
+            if (x.Cost > y.Cost)
+                return -1;
+            else return 0;
+        }
+           
+    }
+}
---
+++ pr13_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr13_1
+{
+     class Program
+    {
+        static void Main()
+        {
+            CompInv<AutoShop> cp = new CompInv<AutoShop>();
+            List<AutoShop> dic = new List<AutoShop>();
+
+            dic.Add(new AutoShop("Toyota Corolla", 180, 300000, 5, 1));
+            dic.Add(new AutoShop("VAZ 2114i", 160, 220000, 0, 2));
+            dic.Add(new AutoShop("Daewoo Nexia", 140, 260000, 5, 3));
+            dic.Add(new AutoShop("Honda Torneo", 220, 400000, 7, 4));
+            dic.Add(new AutoShop("Audi R8 Best", 360, 42000000, 3, 5));
+
+
+
+            Console.WriteLine("Исходный каталог автомобилей: \n");
+            Console.WindowWidth = 100;
+            foreach (AutoShop a in dic) 
+                Console.WriteLine(a);
+
+            Console.WriteLine("\nТеперь автомобили отсортированны по стоимости: \n");
+            dic.Sort(cp);
+            foreach (AutoShop a in dic)
+                Console.WriteLine(a);
+
+            Console.ReadLine();
+
+        }
+    }
+}
